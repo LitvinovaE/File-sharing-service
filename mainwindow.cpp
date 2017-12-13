51e36771b6d591e18ac694bfe33f78b4491ed8bb
LitvinovaE <litvinova.eugeniya@yandex.ru>
2017-12-13 23:28:16 +0300

    MainWindow update

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,7 +7,7 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
 
     ui->table_widget->setRowCount(1);                              // draw a table
     ui->table_widget->setColumnCount(4);
-    ui->table_widget->setColumnWidth(0, 572); /* another value is 539 if more than */
+    ui->table_widget->setColumnWidth(0, 572);
     ui->table_widget->setColumnWidth(1, 100);
     ui->table_widget->setColumnWidth(2, 100);
     ui->table_widget->setColumnWidth(3, 170);
@@ -19,12 +19,61 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_button_my_requests_clicked()
+void MainWindow::on_button_find_clicked()       //TO DO!!!
+{
+    QString file_mask = ui->file_mask_edit->text();
+    if(file_mask.isEmpty())
+    {
+        QToolTip::showText(ui->file_mask_edit->mapToGlobal(QPoint()), tr("Please, enter the mask of files"));
+    }
+    QString **file_table = new QString* [30];
+    int num_elem = 30;
+    for(int j = 0; j < num_elem; j++)
+    {
+        QString *str = new QString [4];
+        str[0] = "NNNNAmmmmme";
+        str[1] = "456.25";
+        str[2] = "13.12.2017";
+        str[3] = "John1234";
+        file_table[j] = str;
+    }
+    draw_table(file_table, num_elem);
+}
+
+void MainWindow::draw_table(QString **file_table, int num_elem)
+{
+    ui->table_widget->setRowCount(num_elem);
+    if(num_elem > 21)
+        ui->table_widget->setColumnWidth(0, 547);
+    else
+        ui->table_widget->setColumnWidth(0, 563);
+    int row_number = ui->table_widget->rowCount();
+    int column_number = ui->table_widget->columnCount();
+    for(int i = 0; i < row_number; i++)
+        for(int j = 0; j < column_number; j++)
+        {
+            QTableWidgetItem *item = new QTableWidgetItem(tr("%1").arg(file_table[i][j]));
+            ui->table_widget->setItem(i, j, item);
+        }
+}
+
+void MainWindow::on_actionChange_download_share_folders_triggered()
 {
 
 }
 
-void MainWindow::on_button_find_clicked()
+void MainWindow::on_actionChange_login_triggered()
+{
+    Login w;
+    w.show();
+}
+
+void MainWindow::on_actionAbout_triggered()
 {
 
+}
+
+void MainWindow::on_actionCreators_triggered()
+{
+    QMessageBox::information(0, "Creators", "This project is created by:\n\nBondarev Mihail\nLitvinova Evgeniya\nOrlov Oleg\n\n2017, MIPT");
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,8 +1,12 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "login.h"
 #include <QMainWindow>
 #include <QVector>
+#include <QString>
+#include <QToolTip>
+#include <QMessageBox>
 
 namespace Ui {
 class MainWindow;
@@ -17,12 +21,15 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_button_my_requests_clicked();
     void on_button_find_clicked();
+    void on_actionChange_download_share_folders_triggered();
+    void on_actionChange_login_triggered();
+    void on_actionAbout_triggered();
+    void on_actionCreators_triggered();
 
 private:
     Ui::MainWindow *ui;
-    void draw_table(QVector<QVector<QString>> table_vectors);
+    void draw_table(QString **file_table, int num_elem);
 };
 
 #endif // MAINWINDOW_H
